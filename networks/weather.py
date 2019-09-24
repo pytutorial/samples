cdbc79e45720a487b89eca6864eeb0145b61c139
pytutorial <51279995+pytutorial@users.noreply.github.com>
2019-09-24 11:58:37 +0700

    Update weather.py

--- networks/weather.py
+++ networks/weather.py
@@ -1,10 +1,8 @@
 import requests, json
 
-url = "http://api.openweathermap.org/data/2.5/forecast?id=1581129&units=metric&appid=d6477696b63c2e661af64eead58c11d9&cnt=8"
-#url = "http://api.openweathermap.org/data/2.5/weather?id=1581129&units=metric&appid=d6477696b63c2e661af64eead58c11d9"
+#url = "http://api.openweathermap.org/data/2.5/forecast?id=1581129&units=metric&appid=d6477696b63c2e661af64eead58c11d9&cnt=8"
+url = "http://api.openweathermap.org/data/2.5/weather?id=1581129&units=metric&appid=d6477696b63c2e661af64eead58c11d9"
 result = requests.get(url).text
 
 data = json.loads(result)
 print(json.dumps(data, indent=4))
-for item in data['list']:
-    print(item['dt_txt'], ':',  item['main']['temp'], 'độ C')
