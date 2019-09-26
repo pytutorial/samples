dba4f58a89e585438607dfbef5b06add875b7dcf
tungdt <duongthanhtungvn01@gmail.com>
2019-09-26 20:10:23 +0700

    Update

--- database/select.sql
+++ database/select.sql
@@ -1,4 +1,6 @@
 SELECT * FROM student;
+SELECT * FROM student LIMIT 1000;
+SELECT * FROM student LIMIT 1,10;
 SELECT * FROM student WHERE student_no='1001';
 SELECT student_no, name FROM student WHERE NAME LIKE '%Văn A%';
 SELECT student_no, name, gpa FROM student WHERE gpa >= 7.0 AND address = 'Hà Nội';
