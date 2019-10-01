3c13835a9674dd69020fe64eab5e5c8c2fe0ca9a
dttvn0010 <39404278+dttvn0010@users.noreply.github.com>
2019-10-01 19:29:41 +0700

    Create join.sql

---
+++ database/join.sql
@@ -0,0 +1,6 @@
+SELECT * FROM product where id=5251
+SELECT * FROM category where id=4741
+
+SELECT product.id, product.name as product, list_price, category.name as category 
+    FROM product JOIN category ON product.categ_id=category.id 
+WHERE product.id=5251
