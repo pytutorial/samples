26c91a903d8bffcacfdaf28ec1e608e2a1490bf1
dttvn0010 <39404278+dttvn0010@users.noreply.github.com>
2019-10-01 19:02:58 +0700

    Update create_fk.sql

--- database/create_fk.sql
+++ database/create_fk.sql
@@ -1,1 +1,16 @@
+CREATE TABLE category(
+    id BIGINT NOT NULL AUTO_INCREMENT,
+    parent_id BIGINT,
+    name VARCHAR(200),
+    PRIMARY KEY(id)) 
+DEFAULT CHARACTER SET=utf8;
+
+CREATE TABLE product(
+    id BIGINT NOT NULL AUTO_INCREMENT,
+    categ_id BIGINT,
+    name VARCHAR(200),
+	list_price DECIMAL,
+    PRIMARY KEY(id)) 
+DEFAULT CHARACTER SET=utf8;
+
 ALTER TABLE product ADD CONSTRAINT fk_product_categ_id FOREIGN KEY (categ_id) REFERENCES category(id);
