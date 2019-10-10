3d6a78533e05dee5a8d1f04074a58ea2c49fdff7
tungdt <duongthanhtungvn01@gmail.com>
2019-10-10 15:02:36 +0700

    Update

--- pandas/pd_data_analysis.py
+++ pandas/pd_data_analysis.py
@@ -13,33 +13,13 @@ print('\nTotal revenue : ', df['revenue'].sum())
 #======================================== Daily revenues =====================================
 
 df_date = df.groupby('date')['revenue'].sum()
-df_date = pd.DataFrame({'date' : df_date.index, 
-                        'revenue' : df_date.values.astype('int')})
-
                         
 print('\nRevenue for every day:')
 print(df_date)
 
 #======================================== Stores' revenues ===================================
 df_store = df.groupby('location_id')['revenue'].sum()
-df_store = pd.DataFrame({'location_id' : df_store.index, 
-                         'revenue' : df_store.values.astype('int')})
-
-df_store = df_store.sort_values(by='revenue', ascending=False)
-df_store = df_store.reset_index(drop=True)
+df_store = df_store.sort_values(ascending=False)
 print('\nRevenue for each store :')
 print(df_store)
-
-#======================================== Average revenues for categories======================
-
-df_categ = df.groupby('categ_id')['revenue'].mean()
-df_categ = pd.DataFrame({'categ_id' : df_categ.index, 
-                           'revenue' : df_categ.values.astype('int')})
-                           
-df_categ = df_categ.sort_values(by='revenue', ascending=False)
-df_categ = df_categ.reset_index(drop=True)
-print('\nAverage revenue of product in categories : ')
-print(df_categ)
-
-
 
