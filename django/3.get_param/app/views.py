1f8b6ca303420dd9eab4bbd687313de3f334fdbd
dttvn0010 <39404278+dttvn0010@users.noreply.github.com>
2020-09-10 02:46:51 -0400

    Update views.py

--- django/3.get_param/app/views.py
+++ django/3.get_param/app/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 
 def login(request):
     name = request.GET.get('name','')
-    request.sesion['name'] = name
+    request.session['name'] = name
     
 def index(request): 
     name = request.session.get('name', '')
