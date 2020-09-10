708788ac231b7b68b149ced3b5041eb942b71065
dttvn0010 <39404278+dttvn0010@users.noreply.github.com>
2020-09-10 02:46:03 -0400

    Update views.py

--- django/3.get_param/app/views.py
+++ django/3.get_param/app/views.py
@@ -4,7 +4,7 @@ def login(request):
     name = request.GET.get('name','')
     request.sesion['name'] = name
     
-def hello(request): 
+def index(request): 
     name = request.session.get('name', '')
     return HttpResponse('Hello ' + name)
    
