ef4fdd14ddd0a4caa61887df9c05b479201a6a29
dttvn0010 <39404278+dttvn0010@users.noreply.github.com>
2020-10-14 15:04:53 +0100

    Update select.sql

--- database/select.sql
+++ database/select.sql
@@ -4,3 +4,5 @@ SELECT * FROM student LIMIT 1,10;
 SELECT * FROM student WHERE student_no='1001';
 SELECT student_no, name FROM student WHERE NAME LIKE '%Văn A%';
 SELECT student_no, name, gpa FROM student WHERE gpa >= 7.0 AND address = 'Hà Nội';
+SELECT student_no, name, gpa FROM student WHERE gpa >= 7.0 AND address = 'Hà Nội' ORDER BY gpa;
+SELECT student_no, name, gpa FROM student WHERE gpa >= 7.0 AND address = 'Hà Nội' ORDER BY gpa DESC;
